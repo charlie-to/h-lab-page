a1bc0b8bc012397f9fbe4031236bcbeb90201bd8
charlie <62843186+charlie-to@users.noreply.github.com>
2024-12-16 19:29:27 +0900

    fix astro config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://charlie-to.github.io",
+  base: "/h-lab-page",
   output: "static",
   integrations: [icon(), tailwind()],
   i18n: {
