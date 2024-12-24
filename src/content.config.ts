51351bde11afd90c9ad2801fd2c9ce3a7642315c
charlie <62843186+charlie-to@users.noreply.github.com>
2024-12-24 12:38:34 +0900

    add contents
    add content collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,30 @@
-import { z, defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const news = defineCollection({
-    loader: glob({ pattern: '**/**/*.md', base: './src/content/news/' }),
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-    }),
+  loader: glob({ pattern: "**/**/*.md", base: "./src/content/news/" }),
+  schema: z.object({
+    title: z.string(),
+    lead: z.string(),
+    date: z.date(),
+  }),
 });
 
 const members = defineCollection({
-    loader: glob({ pattern: '*.md', base: './src/content/member/' }),
-    schema: ({image}) => z.object({
-        name: z.string(),
-        name_en: z.string().optional(),
-        image: image().optional(),
-        history: z.array(z.object({
+  loader: glob({ pattern: "*.md", base: "./src/content/member/" }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      name_en: z.string().optional(),
+      image: image().optional(),
+      history: z
+        .array(
+          z.object({
             year: z.number(),
             content: z.array(z.string()),
-        })).optional(),
+          }),
+        )
+        .optional(),
     }),
 });
 
