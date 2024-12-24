c1c260d7a1ffd67fba6aa9abeb939e55a0a543c3
charlie <62843186+charlie-to@users.noreply.github.com>
2024-12-24 12:37:48 +0900

    add typegraphy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,12 @@
 import daisyui from "daisyui";
+import typography from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [daisyui, typography],
   daisyui: {
     themes: ["emerald"],
   },
